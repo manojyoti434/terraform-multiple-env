c150e94a93ec6b3d6d02e6e87e590c2725c87231
mano jyoti <manojyoti434@gmail.com>
2025-07-10 16:13:42 +0530

    terraform

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,7 @@
+
+bucket = "84s-remote-state-mano-dev"
+key    = "tfvars-demo"
+region = "us-east-1"
+encrypt        = true
+use_lockfile = true
+
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,8 @@
+
+
+bucket = "84s-remote-state-mano-prod"
+key    = "tfvars-demo"
+region = "us-east-1"
+encrypt        = true
+use_lockfile = true
+
